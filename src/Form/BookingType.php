28191f618c9466c1dea31c1873e4439233e2b95e
Flo <flo@MacBook-Pro-de-Flo.local>
2020-03-13 08:12:14 +0100

    bookings

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use App\Form\ApplicationType;
+use App\Form\DataTransformer\FrenchToDateTimeTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -11,6 +12,12 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
 {
+    private $transformer;
+    public function __construct(FrenchToDateTimeTransformer $transformer) 
+    {
+        $this->transformer = $transformer;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -18,6 +25,10 @@ class BookingType extends ApplicationType
             ->add('endDate', TextType::class, $this->getConfiguration("Date de départ","Date à laquelle vous partez"))
             ->add('comment', TextareaType::class, $this->getConfiguration(false, "Si vous avez un commentaire, n'éhistez pas à en faire part.", ['required' => false]))
         ;
+
+        $builder->get('startDate')->addModelTransformer($this->transformer);
+        $builder->get('endDate')->addModelTransformer($this->transformer);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
